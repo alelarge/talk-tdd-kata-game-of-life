397c4af73dfd12e7f84dfec8eabfacfa1bc5fa58
alelarge <armelle.lelarge@shodo.io>
2023-08-30 09:46:33 +0200

    test failed cell should be alive when initialized as alive no compile

--- src/test/kotlin/GameOfLifeTest.kt
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Test
+
 class GameOfLifeTest {
+    @Test
+    fun `cell should be alive when initialized as alive`(){
+        val aliveCell = Cell(CellState.ALIVE)
+        assertThat(aliveCell.isAlive()).isTrue
+    }
 }
