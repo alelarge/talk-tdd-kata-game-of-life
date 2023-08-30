89274b57e3f8dd95c95809374b29729480f61d7a
alelarge <armelle.lelarge@shodo.io>
2023-08-30 21:19:09 +0200

    test failed passed cell died when it has less tan 2 live neighbours

--- src/main/kotlin/GameOfLife.kt
+++ src/main/kotlin/GameOfLife.kt
@@ -5,14 +5,18 @@ class GameOfLife {
         DEAD
     }
 
-    class Cell (private var cellState: CellState) {
+    class Cell (cellState: CellState) {
+
         private var state = cellState
         fun isAlive(): Boolean {
             return state == CellState.ALIVE
         }
 
-        fun evolve(neighbours: List<GameOfLife.Cell>) {
-
+        fun evolve(neighbours: List<Cell>) {
+            val liveNeighbours = neighbours.count {it.state == CellState.ALIVE}
+            if (liveNeighbours < 2) {
+                state = CellState.DEAD
+            }
         }
 
     }
