92db9654cac3285076a21139470ebef7d0f23195
alelarge <armelle.lelarge@shodo.io>
2023-08-30 20:40:47 +0200

    test failed cell died when it has less tan 2 live neighbours no compile

--- src/test/kotlin/GameOfLifeTest.kt
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -13,4 +13,15 @@ class GameOfLifeTest {
         val deadCell = GameOfLife.Cell(GameOfLife.CellState.DEAD)
         assertThat(deadCell.isAlive()).isFalse
     }
+
+    @Test
+    fun `cell dies when it has less than 2 live neighbours`(){
+        val aliveCell = GameOfLife.Cell(GameOfLife.CellState.ALIVE)
+        val neighbours = listOf(GameOfLife.Cell(GameOfLife.CellState.ALIVE), GameOfLife.Cell(GameOfLife.CellState.DEAD), GameOfLife.Cell(
+            GameOfLife.CellState.DEAD
+        )
+            aliveCell.evolve(neighbours)
+            assertThat(aliveCell.isAlive()).isFalse
+        )
+    }
 }
