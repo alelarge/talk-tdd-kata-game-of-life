2c65e8fa08079c9b0a4e5d5c7d66ed791df80e17
alelarge <armelle.lelarge@shodo.io>
2023-09-06 15:52:41 +0200

    test failed at the beginning of the game game of life, if you have living cells, the game continues

--- src/test/kotlin/GameOfLifeTest.kt
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -87,7 +87,12 @@ class CellTest {
         assertThat(game.isOver()).isTrue
     }
 
-
+    @Test
+    fun `at the beginning of the game game of life, if you have living cells, the game continues`(){
+        val grid = Grid(3, 3)
+        val game = Game(grid, listOf(Position(0,0)))
+        assertThat(game.isOver()).isFalse
+    }
 }
 
 
